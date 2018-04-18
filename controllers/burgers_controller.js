184b97e9063fe9e4a9498f4c6543853993f4c579
BrittanyG <brin.greaner@gmail.com>
2018-04-18 14:39:05 -0500

    sequelized

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,19 +1,23 @@
+//import { resolveNaptr } from "dns";
+
 // Inside the burgers_controller.js file, import the following:
 
 // Express
 // burger.js
 
 // Create the router for the app, and export the router at the end of your file.
-
+var connection = require("../config/connection.js");
 var express = require('express');
 // body-parser
 
-var burger = require('../models/burger.js');
+var db = require('../models');
 
 var router = express.Router();
 
-router.get("/", function (req, res){
-    burger.selectAll(function(data){
+
+
+router.get("/", function (req, res) {
+    db.Burger.findAll({}).then(function (data) {
         var hbsObject = {
             burger: data
         };
@@ -22,43 +26,47 @@ router.get("/", function (req, res){
     });
 });
 
-router.post("/api/burgers", function(req, res){
+router.post("/api/burgers", function (req, res) {
     console.log("in put handler")
 
-    burger.insertOne([
-        "burger_name"
-    ], [
-        req.body.burger_name, req.body.devoured
-    ], function (result) {
+    db.Burger.create({
+        burger_name: req.body.burger_name
+    }).then(function (result) {
         // send back the id of the new quote
-        res.json({ id: result.insertId});
-    });
-});
+        res.json({
+            id: result.insertId
+        });
+    })
+})
 
-router.put("/api/burger/:id", function(req, res) {
+router.put("/api/burger/:id", function (req, res) {
     var condition = "id = " + req.params.id;
 
     console.log("condition", condition);
 
     if (req.body.devoured == 1) {
         req.body.devoured = 0
-    }
-    else {
+    } else {
         req.body.devoured = 1
     };
 
-    burger.updateOne("burgers", 
-        req.body.devoured
-        ,
-        req.params.id, function(result) {
+    db.Burger.update({
+        devoured: 1},
+        {
+            where: {
+                id: req.params.id
+            }
+        },
+        ).then(
+        function (result) {
             console.log(JSON.stringify("controller file: " + result))
-        if (result.changedRows == 0 ) {
-            // if no rows were changed, then the id must not exist, so 404
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    });
+            if (result.changedRows == 0) {
+                // if no rows were changed, then the id must not exist, so 404
+                return res.status(404).end();
+            } else {
+                res.json(result)
+            }
+        });
 });
 
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -3,27 +3,15 @@
 // Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
 // Export at the end of the burger.js file.
 
-var orm = require("../config/orm.js");
-
-var burger = {
-    selectAll: function (cb) {
-        orm.selectAll("burgers", function (res) {
-            cb(res);
-            // console.log("orm: " + orm)
-        });
-    },
-    //the variables cols and vals are arrays.
-    insertOne: function(cols, vals, cb) {
-        orm.insertOne("burgers", cols, vals, function(res){
-            cb(res);
-        });
-    },
-    updateOne: function(table, objColVals, condition, cb){
-        console.log("in burger.js model")
-        orm.updateOne("burgers", objColVals, condition, function(res) {
-            cb(res);
-        });
-    }
-};
-
-module.exports = burger;
+module.exports = function (sequelize, DataTypes) {
+    var Burger = sequelize.define("Burger", {
+        burger_name: {
+            type: DataTypes.STRING
+        },
+        devoured: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: 0
+        }
+    })
+    return Burger;
+}
